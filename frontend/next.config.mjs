62df56c91fcbe2dd3d4de3efa55a9dc7d90bac50
Merilyn Kuo <merilynk02@gmail.com>
2024-12-04 13:32:35 -0800

    backend to frontend works!

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    rewrites: async () => {
+        return [
+          {
+            source: '/backend/:path*',
+            destination:
+              process.env.NODE_ENV === 'development'
+                ? 'http://127.0.0.1:8080/backend/:path*'
+                : '/backend',
+          },
+        ]
+      },
+};
 
 export default nextConfig;
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -14,62 +14,28 @@ export default function Home() {
   });
 
   useEffect(() => {
-    fetch("http://127.0.0.1:8080/data").then((response) =>{
+    console.log("Backend URL:", process.env.NEXT_PUBLIC_BACKEND_URL);
+    fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/data`).then((response) => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
       console.log(response);
       response.json().then((data) => {
+        
         console.log(data);
         setData({
           name: data.Name,
           age: data.Age,
           date: data.Date,
           programming: data.programming,
-        });
-      })});
+        });  
+      })
+      .catch((error) => console.error("Error fetching data:", error));});
   }, []);
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
         <div className="App">
             <header className="App-header">
                 <h1>React and flask data</h1>
@@ -81,50 +47,6 @@ export default function Home() {
             </header>
         </div>
       </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
