ae6c44ed1c57a87c27df7cea6147c81021ed6143
Merilyn Kuo <merilynk02@gmail.com>
2025-01-11 12:17:46 -0800

    fixed id synonym issue

--- backend/app/events/utils.py
+++ backend/app/events/utils.py
@@ -4,13 +4,13 @@ from app.extensions import db
 from app.models import Event
 from app.models import User
 
-def get_event_by_uid(uid):
-    event = db.session.query(Event).filter_by(uid=uid).first()
+def get_event_by_uid(urlId):
+    event = db.session.query(Event).filter_by(urlId=urlId).first()
     return event
 
 def create_event(name, coordinator, start, end, earliest, latest, length, numParticipants=None, users=None):
     event = Event(name, coordinator, start, end, earliest, latest, length, numParticipants, users[coordinator])
-    event.uid = generate_uid()  # some unique id generator
+    event.urlId = generate_uid()  # some unique id generator
     db.session.add(event)
     db.session.commit()
 
@@ -21,21 +21,21 @@ def add_user_to_event(self: Event, user: User):
         return True
     return False
 
-def generate_uid():
+def generate_urlId():
     events = Event.query.all()
-    uid_list = []
+    urlId_list = []
     for event in events:
-        uid_list.append(event.uid)
+        urlId_list.append(event.uid)
 
-    uid = ""
-    while uid in uid_list and uid != "":
+    urlId = ""
+    while urlId in urlId_list and urlId != "":
         for _ in range(7):
             var = random.randint(0, 2)
             if var == 0:
-                uid += random.randint(0, 9)
+                urlId += random.randint(0, 9)
             else:    
-                uid += random.choice(string.ascii_letters)
+                urlId += random.choice(string.ascii_letters)
 
-    return uid
+    return urlId
 
 
