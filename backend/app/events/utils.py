938b95d2084a86ada1be6a6567438abeee3227e3
Yoni Dayagi <yonid@MacBookPro.attlocal.net>
2025-05-24 00:51:09 -0700

    Refactor Event model and related functions to use 'uid' instead of 'urlId' for unique identifier. Updated get_event_by_uid and generate_uid functions accordingly.Revert "fixed id synonym issue"
    This reverts commit ae6c44ed1c57a87c27df7cea6147c81021ed6143.

--- backend/app/events/utils.py
+++ backend/app/events/utils.py
@@ -4,8 +4,8 @@ from app.extensions import db
 from app.models import Event
 from app.models import User
 
-def get_event_by_uid(urlId):
-    event = db.session.query(Event).filter_by(urlId=urlId).first()
+def get_event_by_uid(uid):
+    event = db.session.query(Event).filter_by(uid=uid).first()
     return event
 
 def create_event(name, coordinator, start, end, earliest, latest, length, numParticipants=None, users=None):
@@ -21,21 +21,21 @@ def add_user_to_event(self: Event, user: User):
         return True
     return False
 
-def generate_urlId():
+def generate_uid():
     events = Event.query.all()
-    urlId_list = []
+    uid_list = []
     for event in events:
-        urlId_list.append(event.uid)
+        uid_list.append(event.uid)
 
-    urlId = ""
-    while urlId in urlId_list and urlId != "":
+    uid = ""
+    while uid in uid_list and uid != "":
         for _ in range(7):
             var = random.randint(0, 2)
             if var == 0:
-                urlId += random.randint(0, 9)
+                uid += random.randint(0, 9)
             else:    
-                urlId += random.choice(string.ascii_letters)
+                uid += random.choice(string.ascii_letters)
 
-    return urlId
+    return uid
 
 
